677dcd1d7f40fe82d6b3b946e1fd4b2602fa09dc
devcontenidosinteractivos <200440498+devcontenidosinteractivos@users.noreply.github.com>
2026-01-11 11:55:25 -0500

    tema 3 ok

--- src/views/curso/Tema3.vue
+++ src/views/curso/Tema3.vue
@@ -1,10 +1,197 @@
 <template lang="pug">
 .curso-main-container.pb-3
-  BannerInterno(:subTitulo="'3. Título tema'")
+  BannerInterno(:subTitulo="'3. Retención del talento y bienestar organizacional'")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.overflow-hidden
 
+    p.mb-5 Es un factor estratégico para las organizaciones, porque influye directamente en su sostenibilidad y productividad. Cuando se apoya con un enfoque sólido de bienestar organizacional, se fortalecen los niveles de compromiso, desempeño y satisfacción laboral (Patiño Oleas & Sánchez, 2024). El bienestar en el trabajo abarca dimensiones físicas, emocionales y sociales que configuran un entorno saludable y motivador.
+
+    .row.mb-5
+      .col-lg-4.mb-3.mb-lg-0
+        .bg-color-5.tarjeta.p-3.h-100(data-aos="fade-left")
+          p(data-aos="fade-left") Según Peranzola y Troilo (2023), “además de los incentivos económicos, las oportunidades de desarrollar el conocimiento, el feedback [...] y los factores relacionados con la sustentabilidad y el entorno laboral, son claves para la retención” (p. 226). Esto reafirma que la retención exige acciones integrales: compensación justa, desarrollo profesional, reconocimiento continuo y un ambiente positivo.
+          figure.d-flex.justify-content-end
+            img(src="@/assets/curso/tema3/2.svg", style="max-width: 90px")
+      .col-lg-4.mb-3.mb-lg-0
+        figure
+          img(src="@/assets/curso/tema3/1.png" data-aos="zoom-in")
+      .col-lg-4.mb-3.mb-lg-0
+        .bg-color-1.tarjeta.p-3.h-100(data-aos="fade-left")
+          p(data-aos="fade-left") Las estrategias de retención también deben contemplar el equilibrio entre vida personal y laboral. Un estudio en la industria tecnológica, muestra que la falta de políticas flexibles incrementa la rotación, mientras que el teletrabajo y condiciones adaptadas reducen el estrés y elevan la percepción de bienestar (Bonilla, 2022). Esta visión integral promueve en el estudiante el reconocimiento de la importancia de políticas inclusivas y empáticas, así como su capacidad para diseñarlas y evaluarlas.
+    
+    .bg-full-width.bg-color-3.mb-5
+      .p-4.p-md-5.py-4.text-white
+        p.mb-0 Asimismo, el bienestar psicológico sustenta la lealtad organizacional. Investigaciones recientes destacan que un entorno con buen person–organization fit y balance vida–trabajo, fortalece la fidelización de los colaboradores (Patiño Oleas & Sánchez, 2024; Adeoye & Hope, 2020). De esta manera, el enfoque en bienestar no es solo una estrategia humana, sino también un pilar para la retención efectiva del talento. 
+
     #t_3_1.titulo-segundo(data-aos="flip-up")
-      h2 #[span 3.1] Subtítulo tema
+      h2 #[span 3.1] Políticas de compensación, incentivos y beneficios
+    
+    p.mb-5 A continuación conozcamos estos importantes aspectos:
+
+    .bg-full-width.bg-fondo-slider.mb-5
+      .p-4.p-md-5
+        SlyderA(tipo="b").bg-white
+          .row.align-items-center.p-4.p-md-5
+            .col-lg-5.mb-3.mb-lg-0
+              figure
+                img(src="@/assets/curso/tema3/3.png")
+            .col-lg-7
+              h5 Políticas de retribución
+              p Constituyen el núcleo del reconocimiento y la motivación en el ámbito laboral, incorporando elementos tanto monetarios como no monetarios para mantener la competitividad y la retención del talento. Incluyen salario base, bonos, incentivos por desempeño, y beneficios institucionales como seguros, teletrabajo o servicios educativos, que reflejan una visión integral del bienestar humano.
+    
+          .row.align-items-center.p-4.p-md-5
+            .col-lg-5.mb-3.mb-lg-0
+              figure
+                img(src="@/assets/curso/tema3/4.png")
+            .col-lg-7
+              h5 Estímulos
+              p Ya sean tangibles o simbólicos, se diseñan para fomentar conductas que concuerden con los objetivos de la organización y potenciar el compromiso individual. En este sentido, las políticas bien estructuradas estimulan las habilidades blandas, tales como la responsabilidad, la colaboración y la ética, al reconocer la contribución individual y colectiva.
+          .row.align-items-center.p-4.p-md-5
+            .col-lg-5.mb-3.mb-lg-0
+              figure
+                img(src="@/assets/curso/tema3/5.png")
+            .col-lg-7
+              h5 Beneficios
+              p Complementan la compensación buscan alinear las necesidades del colaborador con los valores institucionales. Los programas de bienestar, los programas familiares y los esquemas de salud ocupacional, evidencian una dedicación hacia la mejora de la calidad de vida en el lugar de trabajo. Este método tiene un impacto beneficioso en la fidelidad profesional y en la reducción de la rotación.
+    
+    .row.mb-5
+      .col-lg-4.order-lg-2.mb-3.mb-lg-0
+        figure
+          img(src="@/assets/curso/tema3/6.png", data-aos="zoom-in")
+      .col-lg-8.order-lg-1
+        .bg-color-2.px-4.py-5(data-aos="fade-left")
+          p Es importante que estas políticas cumplan con criterios de igualdad interna, competitividad externa y claridad, según la literatura especializada. 
+          o.mb-0 Por ejemplo, en un caso ficticio, una empresa comercializadora implementa un bono trimestral vinculado a la atención al cliente, un seguro de salud complementario y jornadas familiares de integración. Al realizar este procedimiento, se fomenta la adquisición de habilidades interpersonales tales como la comunicación, la empatía y el trabajo en equipo, y se reduce la rotación de colaboradores de menor edad.
+
+    #t_3_2.titulo-segundo(data-aos="flip-up")
+      h2 #[span 3.2] Cultura organizacional y clima laboral
+    
+    p.mb-5 ¿Qué significan estos dos términos?
+
+    .bg-full-width.bg-color-11
+      .px-4.px-md-5.pb-md-3
+        .row.justify-content-center.align-items-stretch.mb-5
+          .col-lg-6.mb-4(data-aos="zoom-in-up")
+            .custom-image-card-2.h-100
+              img.custom-image-card__image(src="@/assets/curso/tema3/7.png" alt="")
+              .custom-image-card__text.p-4
+                h5.mb-2.text-center Cultura organizacional
+                p.mb-0 Representa la personalidad única de una organización, expresada a través de valores, creencias y comportamientos compartidos por sus miembros.
+          .col-lg-6.mb-4(data-aos="zoom-in-down")
+            .custom-image-card-2.h-100
+              img.custom-image-card__image(src="@/assets/curso/tema3/8.png" alt="")
+              .custom-image-card__text.p-4
+                h5.mb-2.text-center Clima laboral
+                p.mb-0 Se refiere a cómo se siente un empleado en su trabajo cada día, incluyendo aspectos como la satisfacción, la comunicación y el liderazgo.
+
+        .bg-color-1.brd.mb-5
+          .px-4.px-md-5.py-4
+            .row.align-items-center
+              .col-lg-auto
+                img(src="@/assets/curso/tema3/9.svg", style="max-width: 90px").mx-auto
+              .col-lg
+                p.mb-0 Estos dos componentes constituyen el entorno que incide directamente en el rendimiento, el compromiso y la retención del talento humano.
+    
+    .row.mb-5
+      .col-lg-4.mb-3.mb-lg-0
+        figure
+          img(src="@/assets/curso/tema3/10.png", data-aos="zoom-in")
+      .col-lg-8
+        p(data-aos="fade-left") Según un estudio de la Alcaldía de Cali (2021), “el clima organizacional engloba las percepciones sobre condiciones físicas y emocionales en el entorno laboral”. Por su parte, la gestión del clima en las PYMES ha demostrado que promover relaciones de confianza y apoyo influye de manera decisiva en la productividad y motivación del personal. De esta forma, se refuerza la idea de que una cultura sólida y un clima positivo potencian habilidades como la empatía, comunicación efectiva y trabajo en equipo.
+    
+        .bg-color-6.p-4.mb-3(data-aos="fade-left")
+          .row.align-items-start
+            .col-lg-auto
+              img(src="@/assets/curso/tema3/11.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              p.mb-0 Para medir estos aspectos se emplean herramientas como encuestas de clima, grupos focales y entrevistas grupales. Estas técnicas permiten identificar retos concretos, por ejemplo, falta de reconocimiento o deficiente liderazgo, y diseñar acciones de mejora, tales como talleres de feedback asertivo o espacios de bienestar emocional, que fortalecen el sentido de pertenencia.
+        p Un ejercicio práctico para estudiantes podría incluir la aplicación de una breve encuesta en un entorno real (una organización universitaria o una empresa local), seguida de la interpretación de los resultados y la propuesta de estrategias de mejoramiento. Este tipo de actividad permite desarrollar habilidades de análisis crítico, escucha activa y trabajo colaborativo.
+    
+    h2(data-aos="fade-left") Ejemplo práctico. Cultura organizacional y clima laboral
+    p La empresa INNOVATIVA S.A.S., dedicada al desarrollo de software educativo, ha detectado en su última encuesta de clima laboral una disminución en el sentido de pertenencia y una percepción negativa sobre la comunicación interna. Los líderes del área de talento humano deciden intervenir para mejorar el ambiente laboral y fortalecer su cultura organizacional.
+    p.mb-5 Intervención realizada:
+
+    .row.align-items-start.mb-5
+      .col-lg-4.order-lg-2.mb-3.mb-lg-0
+        figure
+          img(src="@/assets/curso/tema3/12.png", alt="").mb-4.mb-lg-0
+      .col-lg-8.order-lg-1
+    
+        AcordionA(tipo="b")
+          .div(titulo="Paso 1")
+            p Se realizó un grupo focal con empleados de diferentes áreas, para escuchar de primera mano sus inquietudes.
+    
+          .div(titulo="Paso 2")
+            p Se rediseñó la estrategia de comunicación interna con boletines semanales y reuniones mensuales de equipo.
+    
+          .div(titulo="Paso 3")
+            p Se creó un programa de reconocimiento llamado 'Conectados', en el cual los colaboradores nominan compañeros que representan los valores de la empresa.
+    
+          .div(titulo="Paso 4")
+            p Se implementaron pausas activas y actividades de bienestar emocional dirigidas por expertos.
+          
+          .div(titulo="Paso 5")
+            p Se midió el impacto de la intervención a los 3 meses con una encuesta de seguimiento.
+    
+    .bg-full-width.bg-color-4
+      .px-4.px-md-5.pb-md-
+        .row.mb-5
+          .col-lg-4.mb-3.mb-lg-0
+            figure
+              img(src="@/assets/curso/tema3/13.png", data-aos="zoom-in")
+          .col-lg-8
+            h2.mb-4(data-aos="fade-left") Resultados:
+            .bg-color-9.p-4(data-aos="fade-left")
+              ul.lista-ul--color
+                li.mb-0.d-flex
+                  i.far.fa-arrow-alt-circle-right
+                  p.mb-0 Aumento del 25 % en la percepción de pertenencia.
+                li.mb-0.d-flex
+                  i.far.fa-arrow-alt-circle-right
+                  p.mb-0 Mejora en la satisfacción sobre canales de comunicación (de 6.2 a 8.7 sobre 10).
+                li.mb-0.d-flex
+                  i.far.fa-arrow-alt-circle-right
+                  p.mb-0 Incremento de participación en actividades de integración del 40 % al 85 %.
+            .bg-color-5.p-4(data-aos="fade-left")
+              p.mb-0 Este caso demuestra cómo una intervención sencilla y estratégica puede transformar el clima laboral, reforzar los valores compartidos y activar habilidades blandas como empatía, comunicación efectiva y trabajo colaborativo. La cultura organizacional, cuando se gestiona de forma consciente, se convierte en un motor de bienestar y productividad.
+        
+        h2(data-aos="fade-left") Elementos claves de la cultura organizacional y el clima laboral
+        p.mb-5 El diagnóstico de estos elementos proporciona una visión valiosa para promover entornos de trabajo positivos y productivos.
+
+        div.row.justify-content-center.align-items-stretch.mb-5
+          div.col-lg-3.mb-4(data-aos="zoom-in-up")
+            div.bg-color-white.box-shadow.px-4.py-5.h-100
+              img.mx-auto.d-block.mb-4(src="@/assets/curso/tema3/14.svg" alt="" style="width: 90px")
+              h5.text-center Diagnóstico
+              p.mb-0.text-center Se aplica una encuesta para evaluar la cultura organizacional y el clima laboral de una empresa.
+        
+          div.col-lg-3.mb-4(data-aos="zoom-in-up")
+            div.bg-color-white.box-shadow.px-4.py-5.h-100
+              img.mx-auto.d-block.mb-4(src="@/assets/curso/tema3/15.svg" alt="" style="width: 90px")
+              h5.text-center Análisis de datos
+              p.mb-0.text-center Los resultados muestran falta de reconocimiento y escasa participación de los empleados.
+        
+          div.col-lg-3.mb-4(data-aos="zoom-in-up")
+            div.bg-color-white.box-shadow.px-4.py-5.h-100
+              img.mx-auto.d-block.mb-4(src="@/assets/curso/tema3/16.svg" alt="" style="width: 90px")
+              h5.text-center Plan de acción
+              p.mb-0.text-center Se implementan iniciativas de reconocimiento y dinámicas de colaboración.
+            
+          div.col-lg-3.mb-4(data-aos="zoom-in-up")
+            div.bg-color-white.box-shadow.px-4.py-5.h-100
+              img.mx-auto.d-block.mb-4(src="@/assets/curso/tema3/17.svg" alt="" style="width: 90px")
+              h5.text-center Evaluación
+              p.mb-0.text-center Una medición posterior revela una mejora del bienestar y compromiso del personal.
+    
+    #t_3_3.titulo-segundo(data-aos="flip-up")
+      h2 #[span 3.3] Estrategias para la fidelización, motivación y bienestar integral del talento humano
+    
+    p.mb-5 Para conocer sobre la temática, lo invitamos a ver el siguiente video.
+
+    .bg-full-width.bg-fondo-slider.mb-5
+      .p-4.p-md-5
+        figure
+          .video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/9cqLwqWDCzs?si=F1b2M6SlLRnGqFf9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     .bg-full-width.border-top.color-primario
       .p-4.p-md-5
@@ -15,15 +202,52 @@
 
             p.d-flex.my-4
               img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
-              a(href="https://elibro.net/es/lc/tecnologicadeloriente/titulos/274611" target="_blank" rel="noopener noreferrer") De Castro Emilio Cerdá, F. J. A. L. M. (s/f). Las energías renovables en el ámbito internacional. Revistasice.com. 
+              a(href="https://es.wikipedia.org/wiki/Recursos_humanos" target="_blank" rel="noopener noreferrer") Wikipedia. (2025). Recursos humanos – incentivos. En Wikipedia, la enciclopedia libre.  
+            p.d-flex.my-4
+              img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
+              a(href="https://www.redalyc.org/pdf/646/64619990004.pdf" target="_blank" rel="noopener noreferrer") Redalyc. (2019). Sistema de incentivos y tipos básicos de trabajo en la organización [PDF].  
 
             p.d-flex.my-4
-              img.me-3(src='@/assets/template/book.svg' :style="{'max-width':'16px'}")
-              span De Janeiro, R. (1992). La declaración del Río sobre el Medio Ambiente y el Desarrollo. Río de Janeiro República Federativa del Brasil., aprobada en Estocolmo el, 16.
+              img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
+              a(href="https://www.adr.gov.co/wp-content/uploads/2021/07/Programa-de-Bienestar-Social-e-Incentivos-21-06-2019.pdf" target="_blank" rel="noopener noreferrer") Plan Bienestar e Incentivos. (2021). Programa de Bienestar Social e Incentivos [PDF].  
+
+
+            p.d-flex.my-4
+              img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
+              a(href="https://es.scribd.com/presentation/377121799/Politicas-de-Remuneracion " target="_blank" rel="noopener noreferrer") Scribd. (2021). Políticas de remuneración [Presentación PDF].  
+
+
+            p.d-flex.my-4
+              img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
+              a(href="https://www.researchgate.net/profile/John_Rico/publication/327140043_Importancia_del_Clima_Organizacional_para_la_Competitividad_de_las_Pymes/links/…" target="_blank" rel="noopener noreferrer") Astelio Rico, J. et al. (2018). Importancia del clima organizacional para la competitividad de las PYMES [PDF]. ResearchGate.  
+
+
+            p.d-flex.my-4
+              img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
+              a(href="https://portal.croem.es/Web20/CROEMPrevencionRiesgos.nsf/F2F012B3D1D4A383C125880100311E56/$FILE/Gu%C3%ADa%20Bienestar%20Organizacional%20CROEM.pdf" target="_blank" rel="noopener noreferrer") Instituto Nacional de Seguridad y Salud en el Trabajo (INSST). (2021). Guía de bienestar organizacional en PYMES [PDF].  
+
+
+            p.d-flex.my-4
+              img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
+              a(href="https://repository.uniminuto.edu/bitstreams/c2733510-4db9-4a96-b639-099143166466/download " target="_blank" rel="noopener noreferrer") Trucco’s Jeans. (2024). La motivación y el bienestar laboral como estrategia para potenciar ... [Informe interno]. Universidad Minuto de Dios.  
+
+
+            p.d-flex.my-4
+              img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
+              a(href="https://apps.who.int/iris/bitstream/handle/10665/44466/9789243500249_spa.pdf" target="_blank" rel="noopener noreferrer") Organización Mundial de la Salud (OMS). (2023). Entornos Laborales Saludables: Fundamentos y Modelo de la OMS [PDF].  
+
+
+            p.d-flex.my-4
+              img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
+              a(href="https://www.researchgate.net/publication/373038749_Talento_humano_en_la_era_digital_fidelizacion_de_trabajadores_en_universidades" target="_blank" rel="noopener noreferrer") Documento de investigación. (2024). Fidelización del talento humano en la era digital. ResearchGate.  
+
+
 
             p.d-flex.my-4
               img.me-3(src='@/assets/template/icono-yt.svg' :style="{'max-width':'16px'}")
-              a(href="https://youtu.be/QKLjwCMyv3M?si=uAWhO9tnyghljaJg" target="_blank" rel="noopener noreferrer") Innovación y sostenibilidad.
+              a(href="https://www.youtube.com/watch?v=aOuxexmyhOo  " target="_blank" rel="noopener noreferrer") Psicométricas. (2022, 27 de febrero). Fidelización del talento. [Vídeo] YouTube.          
+
+
 
           .col-12.col-md-6.col-lg-3.offset-lg-1
             figure
